20cf2a2a05dfc5ac451541c5016c6548ff3a8fa1
Cupang5 <khanalfarizzi@hotmail.com>
2022-05-25 21:19:46 +0700

    Initial Commit

---
+++ Shared/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  Shared
+//
+//  Created by Luthfi Khan on 25/05/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
